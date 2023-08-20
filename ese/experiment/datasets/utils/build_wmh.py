reprocessed again to fix slicewise normalization

ese/experiment/datasets/utils/build_wmh.py
@@ -74,9 +74,18 @@ def normalize_image(image):
     image = image.astype(np.float32)
 
     # Normalize the image between 0 and 1
-    normalized_image = (image - np.min(image)) / (np.max(image) - np.min(image))
+    val_range = np.max(image) - np.min(image)
+    # If the image is a constant, just return a zero image
+    if val_range == 0:
+        normalized_image = np.zeros(image.shape)
+    else:
+        normalized_image = (image - np.min(image)) / (np.max(image) - np.min(image))
     
     return normalized_image
+
+
+def normalize_volume(volume):
+    return np.stack([normalize_image(volume[slice_idx, ...]) for slice_idx in range(volume.shape[0])], axis=0)
 
 
 def proc_WMH(
@@ -152,13 +161,20 @@ def proc_WMH(
             if show:
                 # Plot the slices
                 num_segs = len(image_dict["segs"].keys())
-                im_vol = image_dict["img"]
-                for vis_axis in [0, 1, 2]:
+                for major_axis in [0, 1, 2]:
+
+                    # Figure out how to spin the volume.
+                    all_axes = [0, 1, 2]
+                    all_axes.remove(major_axis)
+                    tranposed_axes = tuple([major_axis] + all_axes)
+
+                    # Set up the figure
                     f, axarr = plt.subplots(1, num_segs + 1, figsize=(5 * (num_segs + 1), 5))
 
                     # Do the sliceing
-                    mid_axis_slice = im_vol.shape[vis_axis] // 2 
-                    img_slice = np.take(im_vol, mid_axis_slice, axis=vis_axis)
+                    rotated_img_vol = np.transpose(image_dict["img"], tranposed_axes)
+                    norm_img_vol = normalize_volume(rotated_img_vol)
+                    img_slice = norm_img_vol[128, ...]
                     
                     # Show the image
                     im = axarr[0].imshow(img_slice, cmap='gray')
@@ -168,10 +184,13 @@ def proc_WMH(
                     # Show the segs
                     for an_idx, annotator in enumerate(image_dict["segs"].keys()):
                         seg_vol = image_dict["segs"][annotator]
-                        seg_slice = np.take(seg_vol, mid_axis_slice, axis=vis_axis)
+                        rotated_seg_vol = np.transpose(seg_vol, tranposed_axes)
+                        seg_slice = rotated_seg_vol[128, ...]
+
                         im = axarr[an_idx + 1].imshow(seg_slice, cmap="gray")
                         axarr[an_idx + 1].set_title(annotator)
                         f.colorbar(im, ax=axarr[an_idx + 1], orientation='vertical')
+                        
                     plt.show()  
             
             if save:
@@ -190,7 +209,10 @@ def proc_WMH(
                     # Save your image
                     img_dir = save_root / "image.npy"
                     rotated_img_vol =  np.transpose(image_dict["img"], tranposed_axes)
-                    np.save(img_dir, rotated_img_vol)
+                    normalized_img_vol = normalize_volume(rotated_img_vol) 
+
+                    # Make sure slice is between [0,1] and the correct dtype.
+                    np.save(img_dir, normalized_img_vol)
 
                     for annotator in image_dict["segs"].keys():
                         # This is how we organize the data.
@@ -199,7 +221,6 @@ def proc_WMH(
                             os.makedirs(annotator_dir)
 
                         label_dir = annotator_dir / "label.npy"
-
                         rotated_mask_vol =  np.transpose(image_dict["segs"][annotator], tranposed_axes)
                         np.save(label_dir, rotated_mask_vol)
 

ese/experiment/datasets/wmh.py
@@ -21,7 +21,7 @@ class WMH(ThunderDataset, DatapathMixin):
     split: Literal["train", "cal", "val", "test"] = "train"
     slicing: Literal["dense", "uniform", "midslice"] = "dense"
     slice_batch_size: int = 1
-    version: str = "v0.1"
+    version: str = "v0.2"
     preload: bool = False
     samples_per_epoch: Optional[int] = None
 
@@ -46,6 +46,8 @@ class WMH(ThunderDataset, DatapathMixin):
         # Get the image and mask
         img_vol = subj_dict['image']
         mask_vol = subj_dict['masks'][self.annotator]
+        label_amounts = subj_dict['pixel_proportions'][self.annotator]
+        allow_replacement = self.slice_batch_size > len(label_amounts[label_amounts> 0])
         
         assert img_vol.dtype == np.float32
         assert mask_vol.dtype == np.float32
@@ -53,54 +55,33 @@ class WMH(ThunderDataset, DatapathMixin):
         # Dense slice sampling means that you sample proportional to how much
         # label there is.
         if self.slicing == "dense":
-            all_axes = [0, 1, 2]
-            all_axes.remove(self.axis)
-            dist = np.sum(mask_vol, axis=tuple(all_axes))
-            # If there are not enough non-zero slices, we allow replacement.
-            allow_replacement = self.slice_batch_size > len(dist[dist > 0])
-            # Same the slices proportional to the amount of label.
-            slice_indices = np.random.choice(np.arange(256), size=self.slice_batch_size, p=dist/np.sum(dist), replace=allow_replacement)
-            img_slice = np.take(img_vol, slice_indices, axis=self.axis)
-            mask_slice = np.take(mask_vol, slice_indices, axis=self.axis)
-
+            label_probs = label_amounts / np.sum(label_amounts)
+            slice_indices = np.random.choice(np.arange(256), size=self.slice_batch_size, p=label_probs, replace=allow_replacement)
         # Uniform slice sampling means that we sample all non-zero slices equally.
         elif self.slicing == "uniform":
-            all_axes = [0, 1, 2]
-            all_axes.remove(self.axis)
-            dist = np.sum(mask_vol, axis=tuple(all_axes))
-            chosen_slices = np.random.choice(np.where(dist > 0)[0], size=self.slice_batch_size, replace=False)
-            img_slice = np.take(img_vol, chosen_slices, axis=self.axis)
-            mask_slice = np.take(mask_vol, chosen_slices, axis=self.axis)
-
-        # Otherwise slice both down the middle.
+            slice_indices = np.random.choice(np.where(label_amounts > 0)[0], size=self.slice_batch_size, replace=allow_replacement)
+        # Otherwise slice  down the middle.
         else:
-            img_slice = np.take(img_vol, 128, axis=self.axis)[None]
-            mask_slice = np.take(mask_vol, 128, axis=self.axis)[None]
-
-        def normalize_image(image):
-            min_val = np.min(image)
-            max_val = np.max(image)
-            normalized_image = (image - min_val) / (max_val - min_val)
-            return normalized_image
-
-        # Make sure slice is between [0,1] and the correct dtype.
-        img = normalize_image(img_slice)
-        mask = mask_slice
+            slice_indices = np.array([128])
+        
+        # Data object ensures first axis is the slice axis.
+        img = img_vol[slice_indices, ...]
+        mask = mask_vol[slice_indices, ...]
 
         return torch.from_numpy(img), torch.from_numpy(mask)
 
     @property
     def _folder_name(self):
-        return f"{self.dataset}/thunder_wmh/{self.task}"
+        return f"{self.dataset}/thunder_wmh/{self.version}/{self.task}/{self.axis}"
 
     @property
     def signature(self):
         return {
-            "dataset": self.dataset,
-            "task": self.task,
-            "resolution": self.resolution,
-            "split": self.split,
-            "slicing": self.slicing,
             "annotator": self.annotator,
+            "dataset": self.dataset,
+            "resolution": self.resolution,
+            "slicing": self.slicing,
+            "split": self.split,
+            "task": self.task,
             "version": self.version,
         }