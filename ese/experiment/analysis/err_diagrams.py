4bc3871f7897b1b9a73a63db5cb8b744520ff9e8
Victor Ion Butoi <victor.butoi@gmail.com>
2023-10-13 14:22:34 -0400

    dist plot done, moving onto next analysis

---
+++ ese/experiment/analysis/err_diagrams.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from typing import List, Optional
+from scipy.stats import gaussian_kde
+from pydantic import validate_arguments
+
+
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
+def viz_accuracy_distribution(
+    data_points: List[dict]
+    ) -> None:
+    return None
+
+@validate_arguments(config=dict(arbitrary_types_allowed=True))
+def viz_region_size_distribution(
+    data_points: List[dict],
+    hue: Optional[str] = None,
+    col: Optional[str] = None,
+    row: Optional[str] = None,
+    row_split: Optional[str] = None,
+    ) -> None:
+    # Combine all the subject label dictionaries of region sizes 
+    # in a single dictionary.    
+    region_size_records = [] 
+    for subject in data_points:
+        for labmap_type in ["pred", "gt"]:
+            lab_sizes_dict_key = "gt_lab_region_sizes" if (labmap_type=="gt") else "pred_lab_region_sizes"
+            lab_size_dict = subject[lab_sizes_dict_key]
+            for label in lab_size_dict.keys():
+                for lab_reg_size in lab_size_dict[label]:
+                    record = {
+                        "subject_id": subject['subject_id'],
+                        "label": label,
+                        "label_map_type": labmap_type,
+                        "region_size": lab_reg_size
+                    } 
+                    region_size_records.append(record)
+    # Combine all the subject label dictionaries of region sizes
+    regions_df = pd.DataFrame(region_size_records)
+
+    # Define what a positive KDE looks like.
+    def positive_kde(*args, **kwargs):
+        sns.kdeplot(*args, clip=(0, None), **kwargs)
+
+    # Creating a FacetGrid with KDE plots
+    if row_split is None:
+        g = sns.FacetGrid(
+            regions_df, 
+            hue=hue, 
+            col=col, 
+            row=row, 
+            height=4, 
+            aspect=1.5)
+        g = g.map(positive_kde, "region_size", fill=True)
+        g.add_legend()
+        plt.show()
+    else:
+        for value in regions_df[row_split].unique():
+            print("For {row_split} = {value}".format(row_split=row_split, value=value))
+            g = sns.FacetGrid(regions_df[regions_df[row_split]==value], 
+                              hue=hue, 
+                              col=col, 
+                              row=row, 
+                              height=4, 
+                              aspect=1.5)
+            g = g.map(positive_kde, "region_size", common_norm=False, fill=True)
+            g.add_legend()
+            plt.show()
