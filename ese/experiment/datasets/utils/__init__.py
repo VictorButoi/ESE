6c06dfbd850e5ca4be52980b1bac474c0b3d06f0
Victor Ion Butoi <victor.butoi@gmail.com>
2024-07-09 11:44:00 -0400

    procing acdc to try another dataset

--- ese/experiment/datasets/utils/__init__.py
+++ ese/experiment/datasets/utils/__init__.py
@@ -7,4 +7,5 @@ from .build_drive import thunderify_DRIVE
 from .build_stare import thunderify_STARE 
 from .build_oasis import thunderify_OASIS 
 from .build_ade20k import thunderify_ADE20K 
+from .build_acdc import thunderify_ACDC
 from .build_wmh import thunderify_WMH 
