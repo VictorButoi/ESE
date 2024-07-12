54705a5da73f1a38d92d1567b1dc1fc1f4e283e7
Victor Ion Butoi <victor.butoi@gmail.com>
2024-07-12 14:26:56 -0400

    cleaned up posthoc exp class slightly

--- ese/experiment/experiment/posthoc_exp.py
+++ ese/experiment/experiment/posthoc_exp.py
@@ -20,6 +20,7 @@ from ionpy.experiment.util import absolute_import, eval_config
 import os
 from typing import Optional
 
+
 class PostHocExperiment(TrainExperiment):
 
     def build_data(self, load_data):
@@ -29,48 +30,36 @@ class PostHocExperiment(TrainExperiment):
         # Get the data and transforms we want to apply
         pretrained_data_cfg = self.pretrained_exp.config["data"].to_dict()
         # Update the old cfg with new cfg (if it exists).
-        if "data" in self.config:
-            pretrained_data_cfg.update(self.config["data"].to_dict())
-        total_config["data"] = pretrained_data_cfg
-        autosave(total_config, self.path / "config.yml") # Save the new config because we edited it.
-        self.config = Config(total_config)
-        self.data_class = pretrained_data_cfg["_class"]
-        # Optionally, load the data.
-        if load_data:
-            # Get the dataset class and build the transforms
-            dataset_cls = absolute_import(pretrained_data_cfg.pop("_class"))
-            # Build the augmentation pipeline.
-            aug_exists = "augmentations" in total_config and (total_config["augmentations"] is not None)
-            if "add_aug" in pretrained_data_cfg:
-                add_aug = pretrained_data_cfg.pop("add_aug")
-            else:
-                add_aug = True
-            # If there are augmentations to add and we want to add them, then add them.
-            if aug_exists and add_aug:
-                val_transforms = augmentations_from_config(total_config["augmentations"]["val"])
-                cal_transforms = augmentations_from_config(total_config["augmentations"]["cal"])
-                self.properties["aug_digest"] = json_digest(self.config["augmentations"].to_dict())[
-                    :8
-                ]
-            else:
-                val_transforms, cal_transforms = None, None
-            # Build the datasets, apply the transforms
-            train_splits = pretrained_data_cfg.pop("train_splits")
-            val_splits = pretrained_data_cfg.pop("val_splits")
-            self.train_dataset = dataset_cls(split=train_splits, transforms=val_transforms, **pretrained_data_cfg)
-            self.val_dataset = dataset_cls(split=val_splits, transforms=cal_transforms, **pretrained_data_cfg)
-            # Check if we want to cache the dataset on the GPU.
-            if "cuda" in pretrained_data_cfg:
-                assert pretrained_data_cfg["preload"], "If you want to cache the dataset on the GPU, you must preload it."
-                cache_dsets_on_gpu = pretrained_data_cfg.pop("cuda")
-            else:
-                cache_dsets_on_gpu = False
+        pretrained_data_cfg.update(total_config.get("data", {}))
+        new_data_cfg = pretrained_data_cfg.copy()
+        # Finally update the data config with the copy. 
+        total_config["data"] = new_data_cfg 
 
-            # Optionally cache the datasets on the GPU.
-            if cache_dsets_on_gpu:
-                self.train_dataset = CUDACachedDataset(self.train_dataset)
-                self.val_dataset = CUDACachedDataset(self.val_dataset)
-    
+         # Save the new config because we edited it.
+        autosave(total_config, self.path / "config.yml")
+        self.config = Config(total_config)
+
+        # Get the dataset class and build the transforms
+        dataset_cls = absolute_import(new_data_cfg.pop("_class"))
+        # Build the augmentation pipeline.
+        augmentation_list = total_config.get("augmentations", None)
+        if augmentation_list is not None:
+            train_transforms = augmentations_from_config(augmentation_list.get("train", None))
+            val_transforms = augmentations_from_config(augmentation_list.get("val", None))
+            self.properties["aug_digest"] = json_digest(augmentation_list)[:8]
+        else:
+            train_transforms, val_transforms = None, None
+
+        if load_data:
+            if "train_splits" in new_data_cfg and "val_splits" in new_data_cfg:
+                train_splits = new_data_cfg.pop("train_splits")
+                val_splits = new_data_cfg.pop("val_splits")
+                self.train_dataset = dataset_cls(split=train_splits, transforms=train_transforms, **new_data_cfg)
+                self.val_dataset = dataset_cls(split=val_splits, transforms=val_transforms, **new_data_cfg)
+            else:
+                self.train_dataset = dataset_cls(split="train", transforms=train_transforms, **new_data_cfg)
+                self.val_dataset = dataset_cls(split="val", transforms=val_transforms, **new_data_cfg)
+
     def build_dataloader(self, batch_size=None):
         # If the datasets aren't built, build them
         if not hasattr(self, "train_dataset"):
@@ -87,41 +76,41 @@ class PostHocExperiment(TrainExperiment):
     def build_model(self):
         # Move the information about channels to the model config.
         # by popping "in channels" and "out channesl" from the data config and adding them to the model config.
-        total_config = self.config.to_dict()
-        ###################
-        # BUILD THE MODEL #
-        ###################
+        total_cfg_dict = self.config.to_dict()
+
+        #######################
+        # LOAD THE EXPERIMENT #
+        #######################
         # Get the configs of the experiment
         load_exp_args = {
             "device": "cuda",
             "load_data": False, # Important, we might want to modify the data construction.
+            "checkpoint": total_cfg_dict['train'].get('checkpoint', 'max-val-dice_score')
         }
-        if "checkpoint" in total_config['train']:
-            load_exp_args["checkpoint"] = total_config['train']['checkpoint']
-        else:
-            load_exp_args["checkpoint"] = 'max-val-dice_score' 
             
-        if "config.yml" in os.listdir(total_config['train']['pretrained_dir']):
+        # Either select from a set of experiments in a common directory OR choose a particular experiment to load.
+        if "config.yml" in os.listdir(total_cfg_dict['train']['pretrained_dir']):
             self.pretrained_exp = load_experiment(
-                path=total_config['train']['pretrained_dir'],
+                path=total_cfg_dict['train']['pretrained_dir'],
                 **load_exp_args
             )
         else:
             rs = ResultsLoader()
             self.pretrained_exp = load_experiment(
-                df=rs.load_metrics(rs.load_configs(total_config['train']['pretrained_dir'], properties=False)),
-                selection_metric=total_config['train']['pretrained_select_metric'],
+                df=rs.load_metrics(rs.load_configs(total_cfg_dict['train']['pretrained_dir'], properties=False)),
+                selection_metric=total_cfg_dict['train']['pretrained_select_metric'],
                 **load_exp_args
             )
-        pretrained_cfg = self.pretrained_exp.config.to_dict()
+        # Now we can access the old total config. 
+        pretrained_total_cfg_dict = self.pretrained_exp.config.to_dict()
+
         #########################################
         #            Model Creation             #
         #########################################
-        model_config_dict = total_config['model']
-        if '_pretrained_class' in model_config_dict:
-            model_config_dict.pop('_pretrained_class')
-        self.model_class = model_config_dict['_class']
+        model_cfg_dict = total_cfg_dict['model']
+        pretrained_model_cfg_dict = pretrained_total_cfg_dict['model']
         # Either keep training the network, or use a post-hoc calibrator.
+        self.model_class = model_cfg_dict['_class']
         if self.model_class in ["Vanilla", "FT_CE", "FT_Dice"]:
             self.base_model = torch.nn.Identity()
             # Load the model, there is no learned calibrator.
@@ -130,44 +119,43 @@ class PostHocExperiment(TrainExperiment):
             self.base_model = self.pretrained_exp.model
             self.base_model.eval()
             # Get the old in and out channels from the pretrained model.
-            model_config_dict["num_classes"] = pretrained_cfg['model']['out_channels']
-            model_config_dict["image_channels"] = pretrained_cfg['model']['in_channels']
+            model_cfg_dict["num_classes"] = pretrained_model_cfg_dict['out_channels']
+            model_cfg_dict["image_channels"] = pretrained_model_cfg_dict['in_channels']
             # BUILD THE CALIBRATOR #
             ########################
             # Load the model
-            self.model = eval_config(model_config_dict)
+            self.model = eval_config(model_cfg_dict)
             self.model.weights_init()
-        # Get the tuned calibration parameters.
-        self.properties["num_params"] = num_params(self.model)
+
+
         ########################################################################
         # Make sure we use the old experiment seed and add important metadata. #
         ########################################################################
-        old_exp_config = self.pretrained_exp.config.to_dict() 
-        total_config['experiment'] = old_exp_config['experiment']
-        model_config_dict['_class'] = self.model_class
-        model_config_dict['_pretrained_class'] = parse_class_name(str(self.base_model.__class__))
-        autosave(total_config, self.path / "config.yml") # Save the new config because we edited it.
-        self.config = Config(total_config)
+        # Get the tuned calibration parameters.
+        self.properties["num_params"] = num_params(self.model)
+        # Set the new experiment params as the old ones.
+        total_cfg_dict['experiment'] = pretrained_total_cfg_dict['experiment']
+        # Edit the model_config.
+        total_cfg_dict['model']['_class'] = self.model_class
+        total_cfg_dict['model']['_pretrained_class'] = parse_class_name(str(self.base_model.__class__))
+        # Save the new config because we edited it and reset self.config
+        autosave(total_cfg_dict, self.path / "config.yml") # Save the new config because we edited it.
+        self.config = Config(total_cfg_dict)
     
     def build_loss(self):
-        total_config = self.config.to_dict()
-        if self.model_class in ["FT_CE", "FT_Dice"]:
-            # Load the config for the loss function.
-            loss_cfg_root = Path("/storage/vbutoi/projects/ESE/ese/experiment/configs/losses")
-            ##################################################
-            dataset_name = self.data_class.split(".")[-1]
-            with open(loss_cfg_root / f"{dataset_name}.yaml", 'r') as file:
-                loss_cfg = yaml.safe_load(file)
-            if self.model_class == "FT_CE":
-                new_loss_cfg = loss_cfg["ce_loss"]
-            else:
-                new_loss_cfg = loss_cfg["dice_loss"]
-            ##################################################
-            total_config["loss_func"] = new_loss_cfg
-            autosave(total_config, self.path / "config.yml") # Save the new config because we edited it.
-            self.config = Config(total_config)
-        # Build the loss function.
-        self.loss_func = eval_config(self.config["loss_func"])
+        # If there is a composition of losses, then combine them.
+        # else just build the loss normally.
+        if "classes" in self.config["loss_func"]: 
+            loss_classes = self.config["loss_func"]["classes"]
+            weights = torch.Tensor(self.config["loss_func"]["weights"]).to(self.device)
+            # Get the loss functions you're using.
+            loss_funcs = eval_config(loss_classes)
+            # Get the weights per loss and normalize to weights to 1.
+            loss_weights = (weights / torch.sum(weights))
+            # Build the loss function.
+            self.loss_func = lambda yhat, y: sum([loss_weights[l_idx] * l_func(yhat, y) for l_idx, l_func in enumerate(loss_funcs)])
+        else:
+            self.loss_func = eval_config(self.config["loss_func"])
     
     def run_step(self, batch_idx, batch, backward, **kwargs):
         # Send data and labels to device.
