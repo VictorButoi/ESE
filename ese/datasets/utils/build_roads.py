fe4bd40559c05b36e75499ff81606c119875cc45
Victor Ion Butoi <victor.butoi@gmail.com>
2024-10-08 11:42:26 -0400

    minor bugfix, need seg in 01

--- ese/datasets/utils/build_roads.py
+++ ese/datasets/utils/build_roads.py
@@ -10,7 +10,7 @@ from PIL import Image
 from ionpy.util import Config
 
 # Local imports
-from .utils_for_build import data_splits
+from .utils_for_build import data_splits, normalize_image
 
 
 def thunderify_Roads(
@@ -36,6 +36,7 @@ def thunderify_Roads(
         subjects = [] 
         # Iterate through the examples.
         subj_list = list(os.listdir(image_root))
+        counter = 0
         for example_name in tqdm(os.listdir(image_root), total=len(subj_list)):
             # Define the image_key
             key = "subject_" + example_name.split('_')[0]
@@ -48,8 +49,22 @@ def thunderify_Roads(
             img = np.array(Image.open(img_dir))
             seg = np.array(Image.open(seg_dir))
 
+            # Normalize the seg to be between 0 and 1
+            seg = normalize_image(seg)
+
             # Get the proportion of the binary mask.
             gt_prop = np.count_nonzero(seg) / seg.size
+
+            # # Visualize the image and mask
+            # # if config.get("visualize", False):
+            # fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+            # im = ax[0].imshow(img)
+            # ax[0].set_title("Image")
+            # fig.colorbar(im, ax=ax[0])
+            # se = ax[1].imshow(seg, cmap="gray")
+            # fig.colorbar(se, ax=ax[1])
+            # ax[1].set_title("Mask")
+            # plt.show()
 
             # Move the last channel of image to the first channel
             img = np.moveaxis(img, -1, 0)
@@ -62,6 +77,9 @@ def thunderify_Roads(
                 "gt_proportion": gt_prop 
             } 
             subjects.append(key)
+            counter += 1
+            if counter == 10:
+                break
 
         subjects = sorted(subjects)
         splits = data_splits(subjects, splits_ratio, splits_seed)
