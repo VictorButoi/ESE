f6eeeb032e9820406693f904457d3eae2b9879dc
Victor Ion Butoi <victor.butoi@gmail.com>
2024-10-21 22:14:00 -0400

    reorganized WMH nicely, next to processing

--- ese/datasets/utils/build_wmh.py
+++ ese/datasets/utils/build_wmh.py
@@ -14,177 +14,11 @@ from sklearn.model_selection import train_test_split
 from pydantic import validate_arguments
 
 
-def proc_WMH(
-        data_dirs, 
-        version,
-        show=False, 
-        save=False
-        ):
-
-    proc_root = pathlib.Path(f"/storage/vbutoi/datasets/WMH/processed/{version}") 
-
-    for ud in data_dirs:
-
-        split_args = str(ud).split("/")
-        datacenter = split_args[-2]
-
-        for subj in ud.iterdir():
-            print(subj)
-            image_dict = {}
-            # Get the slices for each modality.
-            image_dir = subj / pathlib.Path(f"pre/FLAIR.nii.gz")
-            img_volume = resample_nib(nib.load(image_dir))
-            # Rotate the volume to be properly oriented
-            rotated_volume = np.rot90(img_volume.get_fdata(), k=3, axes=(2, 0))
-            # Normalize percentile
-            lower = np.percentile(rotated_volume[rotated_volume>0], q=0.5)
-            upper = np.percentile(rotated_volume[rotated_volume>0], q=99.5)
-            clipped_volume = np.clip(rotated_volume, a_min=lower, a_max=upper)
-            # Make the image square
-            max_img_dim = max(clipped_volume.shape)
-            sqr_img = pad_image_numpy(clipped_volume, (max_img_dim, max_img_dim, max_img_dim))
-            # Resize to 256
-            zoom_factors = np.array([256, 256, 256]) / np.array(sqr_img.shape)
-            resized_img = zoom(sqr_img, zoom_factors, order=1)  # You can adjust the 'order' parameter for interpolation quality
-            # Make the type compatible
-            resized_img = resized_img.astype(np.float32)
-            # Store in the dictionary
-            image_dict["img"] = resized_img
-
-            # Get the label slice
-            if "training" in str(ud):
-                label_versions = ["observer_o12", "observer_o3", "observer_o4"]
-            else:
-                label_versions = ["observer_o12"]
-
-            image_dict["segs"] = {}
-            for annotator_name in label_versions:
-
-                # Need to acocunt for extra annotators
-                if annotator_name != "observer_o12":
-                    split_name = str(ud).split("WMH")
-                    alternate_seg = pathlib.Path(split_name[0] + f"/WMH/additional_annotations/{annotator_name}/" + split_name[1] + f"/{subj.name}")
-                    seg_dir = alternate_seg / "result.nii.gz"
-                else:
-                    seg_dir = subj / "wmh.nii.gz"
-
-                seg = resample_mask_to(nib.load(seg_dir), img_volume)
-                # Rotate the volume to be properly oriented
-                rotated_seg = np.rot90(seg.get_fdata(), k=3, axes=(2, 0))
-                # Do the sliceing
-                binary_seg = np.uint8(rotated_seg== 1)
-                # Make the image square
-                max_seg_dim = max(binary_seg.shape)
-                sqr_seg = pad_image_numpy(binary_seg, (max_seg_dim, max_seg_dim, max_seg_dim))
-                # Resize to 256
-                zoom_factors = np.array([256, 256, 256]) / np.array(sqr_seg.shape)
-                resized_seg = zoom(sqr_seg, zoom_factors, order=0)  # You can adjust the 'order' parameter for interpolation quality
-                # Make the type compatible
-                resized_seg = resized_seg.astype(np.float32)
-                # Store in dictionary
-                image_dict["segs"][annotator_name] = resized_seg
-            
-            if show:
-                # Plot the slices
-                num_segs = len(image_dict["segs"].keys())
-                for major_axis in [0, 1, 2]:
-
-                    # Figure out how to spin the volume.
-                    all_axes = [0, 1, 2]
-                    all_axes.remove(major_axis)
-                    tranposed_axes = tuple([major_axis] + all_axes)
-
-                    # Set up the figure
-                    f, axarr = plt.subplots(1, num_segs + 1, figsize=(5 * (num_segs + 1), 5))
-
-                    # Do the sliceing
-                    rotated_img_vol = np.transpose(image_dict["img"], tranposed_axes)
-                    norm_img_vol = normalize_volume(rotated_img_vol)
-                    img_slice = norm_img_vol[128, ...]
-                    
-                    # Show the image
-                    im = axarr[0].imshow(img_slice, cmap='gray')
-                    axarr[0].set_title("Image")
-                    f.colorbar(im, ax=axarr[0], orientation='vertical') 
-                    
-                    # Show the segs
-                    for an_idx, annotator in enumerate(image_dict["segs"].keys()):
-                        seg_vol = image_dict["segs"][annotator]
-                        rotated_seg_vol = np.transpose(seg_vol, tranposed_axes)
-                        seg_slice = rotated_seg_vol[128, ...]
-
-                        im = axarr[an_idx + 1].imshow(seg_slice, cmap="gray")
-                        axarr[an_idx + 1].set_title(annotator)
-                        f.colorbar(im, ax=axarr[an_idx + 1], orientation='vertical')
-                        
-                    plt.show()  
-            
-            if save:
-                for major_axis in [0, 1, 2]:
-                    
-                    # Figure out how to spin the volume.
-                    all_axes = [0, 1, 2]
-                    all_axes.remove(major_axis)
-                    tranposed_axes = tuple([major_axis] + all_axes)
-
-                    save_root = proc_root / datacenter / str(major_axis) / subj.name 
-
-                    if not save_root.exists():
-                        os.makedirs(save_root)
-                    
-                    # Save your image
-                    img_dir = save_root / "image.npy"
-                    rotated_img_vol =  np.transpose(image_dict["img"], tranposed_axes)
-                    normalized_img_vol = normalize_volume(rotated_img_vol) 
-
-                    # Make sure slice is between [0,1] and the correct dtype.
-                    np.save(img_dir, normalized_img_vol)
-
-                    for annotator in image_dict["segs"].keys():
-                        # This is how we organize the data.
-                        annotator_dir = save_root / annotator 
-                        if not annotator_dir.exists():
-                            os.makedirs(annotator_dir)
-
-                        label_dir = annotator_dir / "label.npy"
-                        rotated_mask_vol =  np.transpose(image_dict["segs"][annotator], tranposed_axes)
-                        np.save(label_dir, rotated_mask_vol)
-
-@validate_arguments
-def data_splits(
-    values: List[str], 
-    splits: Tuple[float, float, float, float], 
-    seed: int
-) -> Tuple[List[str], List[str], List[str], List[str]]:
-
-    if len(set(values)) != len(values):
-        raise ValueError(f"Duplicate entries found in values")
-
-    # Super weird bug, removing for now, add up to 1!
-    # if (s := sum(splits)) != 1.0:
-    #     raise ValueError(f"Splits must add up to 1.0, got {splits}->{s}")
-
-    train_size, cal_size, val_size, test_size = splits
-    values = sorted(values)
-    # First get the size of the test splut
-    traincalval, test = train_test_split(values, test_size=test_size, random_state=seed)
-    # Next size of the val split
-    val_ratio = val_size / (train_size + cal_size + val_size)
-    traincal, val = train_test_split(traincalval, test_size=val_ratio, random_state=seed)
-    # Next size of the cal split
-    cal_ratio = cal_size / (train_size + cal_size)
-    train, cal = train_test_split(traincal, test_size=cal_ratio, random_state=seed)
-
-    assert sorted(train + cal + val + test) == values, "Missing Values"
-
-    return (train, cal, val, test)
-
-
-def thunderify_WMH(
-        proc_root, 
-        dst,
-        version
-        ):
+def thunderify_legacy_WMH(
+    proc_root, 
+    dst,
+    version
+):
 
     # Append version to our paths
     proc_root = proc_root / version
@@ -262,6 +96,10 @@ def thunderify_WMH(
                 db["_splits"] = splits
                 db["_attrs"] = attrs
 
+
+############################################################################################################
+# SERIES OF HELPER FUNCTIONS IMPORTANT FOR SETTING UP WMH
+
 def is_integer(s):
     try:
         int(s)
@@ -269,8 +107,8 @@ def is_integer(s):
     except ValueError:
         return False
 
-if __name__=="__main__":
-    root = pathlib.Path("/storage/vbutoi/datasets/WMH")
+def gather_all_subject_dirs():
+    root = pathlib.Path("/storage/vbutoi/datasets/WMH/original_unzipped")
     splits = ["training", "test", "additional_annotations"]
 
     all_dirs = []
@@ -292,8 +130,7 @@ if __name__=="__main__":
                                 if not is_integer(str(l5_dir.name)):
                                     print(l5_dir)
                                     all_dirs.append(l5_dir)
-    
-    all_unique_dirs = []
+    unique_dirs_with_additional = []
     for path in all_dirs:
         all_other_dirs = [p for p in all_dirs if p != path]
         is_subdir = False
@@ -302,6 +139,70 @@ if __name__=="__main__":
                 is_subdir = True
                 break
         if not is_subdir:
-            all_unique_dirs.append(path)
+            unique_dirs_with_additional.append(path)
     
-    unique_dirs = [ud for ud in all_unique_dirs if "additional_annotations" not in str(ud)]
+    return unique_dirs_with_additional
+
+
+# CELL FOR ORGANIZING ORIGINAL DATASET
+def organize_WMH_part1(unique_dirs_with_additional):
+    unique_annotator_o1_dirs = [ud for ud in unique_dirs_with_additional if "additional_annotations" not in str(ud)]
+    organized_dir = "/storage/vbutoi/datasets/WMH/raw_organized"
+
+    for o12_path in unique_annotator_o1_dirs:
+        dataset = o12_path.parts[-2]
+        print(dataset)
+        print(o12_path)
+        # Iterate through the subjects
+        for subject_dir in o12_path.iterdir():
+            subject_id = subject_dir.parts[-1]
+            img_dir = subject_dir / "pre" / "FLAIR.nii.gz"
+            seg_dir = subject_dir / "wmh.nii.gz"
+            print(subject_id, img_dir.exists(), seg_dir.exists())
+            # Now we need to make a copy of the image and segmentation in the organized directory
+            organized_dataset_dir = os.path.join(organized_dir, dataset)
+            new_subject_dir = os.path.join(organized_dataset_dir, subject_id)
+            # Assert that the subject directory does not exist
+            assert not os.path.exists(new_subject_dir)
+            os.makedirs(new_subject_dir)
+            new_img_dir = os.path.join(new_subject_dir, "FLAIR.nii.gz")
+            new_seg_dir = os.path.join(new_subject_dir, "annotator_o12_mask.nii.gz")
+            # Copy the files
+            os.system(f"cp {img_dir} {new_img_dir}")
+            os.system(f"cp {seg_dir} {new_seg_dir}")
+
+
+# CELL FOR ORGANIZING ADDITIONAL ANNOTATIONS
+def organize_WMH_part2(unique_dirs_with_additional):
+
+    other_anno_dirs = [ud for ud in unique_dirs_with_additional if "additional_annotations" in str(ud)]
+    organized_dir = "/storage/vbutoi/datasets/WMH/raw_organized"
+
+    for new_annotator_path in other_anno_dirs:
+        dataset = new_annotator_path.parts[-2]
+        annotator = new_annotator_path.parts[-4]
+
+        if annotator == 'observer_o3':
+            annotator = 'annotator_o3'
+        elif annotator == 'observer_o4':
+            annotator = 'annotator_o4'
+        else:
+            raise ValueError(f"Unknown annotator {annotator}")
+
+        print(dataset)
+        print(annotator)
+        print(new_annotator_path)
+        # Iterate through the subjects
+        for additional_segs_dir in new_annotator_path.iterdir():
+            subject_id = additional_segs_dir.parts[-1]
+            alternate_seg_dir = additional_segs_dir / "result.nii.gz"
+            print(subject_id, alternate_seg_dir.exists())
+            # Now we need to make a copy of the alternative segmentation in the organized directory
+            organized_dataset_dir = os.path.join(organized_dir, dataset)
+            new_subject_dir = os.path.join(organized_dataset_dir, subject_id)
+            # This path should already exist.
+            assert os.path.exists(new_subject_dir)
+            new_annotator_seg_dir = os.path.join(new_subject_dir, f"{annotator}_mask.nii.gz")
+
+            # Copy the files
+            os.system(f"cp {alternate_seg_dir} {new_annotator_seg_dir}")
