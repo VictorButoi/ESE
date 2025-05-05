7a0a3caa687f5a09d4ebac0fd7c0bf395bd24daa
Victor Ion Butoi <victor.butoi@gmail.com>
2025-05-05 10:43:53 -0400

    working through 2D Isles a bit

--- ese/datasets/utils/__init__.py
+++ ese/datasets/utils/__init__.py
@@ -3,7 +3,6 @@ from .build_cityscapes import thunderify_CityScapes
 from .build_RIWA import thunderify_RIWA 
 # from .build_shapes import thunderify_Shapes 
 from .build_octa import thunderify_OCTA
-from .build_isles import thunderify_ISLES
 from .build_lits import thunderify_LiTS
 from .build_drive import thunderify_DRIVE 
 from .build_stare import thunderify_STARE 
@@ -12,3 +11,5 @@ from .build_ade20k import thunderify_ADE20K
 from .build_roads import thunderify_Roads
 from .build_acdc import thunderify_ACDC
 from .build_wmh import thunderify_WMH 
+
+from .build_isles import thunderify_ISLES, nnunetize_ISLES_resave, thunderify_ISLES_frompng
--- ese/datasets/utils/build_isles.py
+++ ese/datasets/utils/build_isles.py
@@ -1,4 +1,5 @@
 # Misc imports
+import json
 import os
 import ast
 import time
@@ -6,11 +7,15 @@ import torch
 import pathlib
 import voxel as vx
 import numpy as np
+from PIL import Image
+import nibabel as nib
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from scipy.ndimage import zoom
 from thunderpack import ThunderDB
 from typing import List, Tuple, Optional
+import imageio.v2 as imageio  # newer versions of imageio
+
 # Ionpy imports
 from ionpy.util import Config
 # Local imports
@@ -188,3 +193,234 @@ def thunderify_ISLES(
         db["_samples"] = subjects
         db["_splits"] = db_splits
         db["_attrs"] = attrs
+
+
+def process_and_save(
+    img_path,
+    seg_path,
+    img_out_path,
+    seg_out_path,
+    pad_to,
+    resize_to,
+    min_label_amount=0,
+    show_examples=False,
+):
+    # --- Load ---
+    # Load the volumes using voxel
+    img = vx.load_volume(img_path)
+    # Load the seg and process to match the image
+    raw_seg_vol = vx.load_volume(seg_path)
+    seg = raw_seg_vol.resample_like(img, mode='nearest')
+    # Get the properties that the image has
+
+    img_data = img.tensor.numpy().squeeze()
+    seg_data = seg.tensor.numpy().squeeze()
+
+    label_amount = np.count_nonzero(seg_data)
+    if label_amount >= min_label_amount:
+
+        img_padded = pad_to_resolution(img_data, pad_to)
+        seg_padded = pad_to_resolution(seg_data, pad_to)
+
+        # --- Resize ---
+        img_resized = resize_volume(img_padded, resize_to)
+        seg_resized = resize_volume(seg_padded, resize_to)
+
+        # Slice the volume by the max slice on the last axis.
+        lab_per_slice = np.sum(seg_resized, axis=(0, 1))
+        max_slice_idx = np.argmax(lab_per_slice)
+
+        img_slice = img_resized[:, :, max_slice_idx]
+        seg_slice = seg_resized[:, :, max_slice_idx]
+
+        # --- Normalize ---
+        img_slice = (normalize_image(img_slice) * 255).astype(np.uint8)
+        # --- Binarize the segmentation ---
+        seg_slice = np.where(seg_slice > 0, 1, 0).astype(np.uint8)
+
+        if show_examples:
+            f, axarr = plt.subplots(1, 2, figsize=(10, 5))
+            im = axarr[0].imshow(img_slice, cmap='gray')
+            axarr[0].set_title("Image")
+            f.colorbar(im, ax=axarr[0])
+            se =axarr[1].imshow(seg_slice, cmap='gray')
+            axarr[1].set_title("Segmentation")
+            f.colorbar(se, ax=axarr[1])
+            plt.show()
+
+        # Write out the image and segmentation
+        imageio.imwrite(img_out_path, img_slice)
+        imageio.imwrite(seg_out_path, seg_slice)
+
+
+def nnunetize_ISLES_resave(
+    cfg,
+    splits: Optional[dict] = {},
+):
+    config = cfg.to_dict()
+
+    proc_root = pathlib.Path(config["root"]) / 'raw_data' / 'ISLES_22'
+    dst_dir = pathlib.Path(config["nnunet_dst_folder"])
+
+    isl_img_root = proc_root / 'cropped_images'
+    isl_seg_root = proc_root / 'unzipped_archive' / 'derivatives'
+
+    out_tr_img_root = dst_dir / 'imagesTr'
+    out_tr_seg_root = dst_dir / 'labelsTr'
+
+    out_ts_img_root = dst_dir / 'imagesTs'
+    out_ts_seg_root = dst_dir / 'labelsTs'
+
+    # Prepare output folders
+    out_tr_img_root.mkdir(parents=True, exist_ok=True)
+    out_tr_seg_root.mkdir(parents=True, exist_ok=True)
+
+    out_ts_img_root.mkdir(parents=True, exist_ok=True)
+    out_ts_seg_root.mkdir(parents=True, exist_ok=True)
+
+    subj_list = list(os.listdir(isl_img_root))
+
+    # Print the total number of training subjects by the len
+    # of the subjects in the splits
+    for split_key in splits:
+        print(f"{split_key}: {len(splits[split_key])} samples")
+
+    for subj_name in tqdm(subj_list, total=len(subj_list)):
+
+        img_path = isl_img_root / subj_name / 'ses-0001' / 'dwi' / f'{subj_name}_ses-0001_dwi_cropped.nii.gz'
+        seg_path = isl_seg_root / subj_name / 'ses-0001' / f'{subj_name}_ses-0001_msk.nii.gz'
+
+        if subj_name not in splits.get("test"):
+            out_img_root = out_tr_img_root
+            out_seg_root = out_tr_seg_root
+        else:
+            out_img_root = out_ts_img_root
+            out_seg_root = out_ts_seg_root
+
+        img_out_path = out_img_root / f'{subj_name}_0000.png'
+        seg_out_path = out_seg_root / f'{subj_name}.png'
+
+        pad_to = ast.literal_eval(config['pad_to']) if isinstance(config['pad_to'], str) else config['pad_to']
+        resize_to = ast.literal_eval(config['resize_to']) if isinstance(config['resize_to'], str) else config['resize_to']
+
+        process_and_save(
+            img_path, 
+            seg_path, 
+            img_out_path, 
+            seg_out_path,
+            pad_to, 
+            resize_to, 
+            min_label_amount=config.get('min_label_amount', 0),
+            show_examples=config.get('show_examples', False)
+        )
+    
+    # Write this out to a json file.
+    json_dict = {
+        "channel_names": {  # formerly modalities
+            "0": "DWI", 
+        }, 
+        "labels": {  # THIS IS DIFFERENT NOW!
+            "background": 0,
+            "foreground": 1,
+        }, 
+        "numTraining": 222,
+        "file_ending": ".png"
+    }
+    json_path = dst_dir / 'dataset.json'
+    with open(json_path, 'w') as f:
+        json.dump(json_dict, f, indent=4)
+
+    print(f"Saved dataset.json to {json_path}")
+
+
+def thunderify_ISLES_frompng(
+    cfg: Config,
+):
+    # Get the dictionary version of the config.
+    config = cfg.to_dict()
+
+    # Append version to our paths
+    version = str(config["version"])
+
+    # Append version to our paths
+    proc_root = pathlib.Path(config["root"])
+    dst_dir = pathlib.Path(config["dst_folder"]) / version
+
+    # Load the splits file
+    split_file = cfg['splits_root'] + "/splits_final.json"
+    with open(split_file, 'r') as f:
+        db_splits = json.load(f)[0]
+    # We need to load the names of all the test files for the final split
+    test_dir = proc_root / 'imagesTs'
+    test_list = [image_name.split("_")[0] for image_name in os.listdir(test_dir)]
+    db_splits["test"] = test_list
+
+    subjects = [] 
+    # Get the subject list by combining the train and test splits
+    subj_list = []
+    for split_key in db_splits:
+        subj_list += db_splits[split_key]
+
+    ## Iterate through each datacenter, axis  and build it as a task
+    with ThunderDB.open(str(dst_dir), "c") as db:
+
+        for subj_name in tqdm(subj_list, total=len(subj_list)):
+
+            # Paths to the image and segmentation
+            if subj_name in db_splits["test"]:
+                img_dir = proc_root / 'imagesTs' / (subj_name + "_0000.png")
+                seg_dir = proc_root / 'labelsTs' / (subj_name + ".png")
+            else:
+                img_dir = proc_root / 'imagesTr' / (subj_name + "_0000.png")
+                seg_dir = proc_root / 'labelsTr' / (subj_name + ".png")
+
+            # Get the tensors from the vol objects
+            img_vol_arr = np.array(Image.open(img_dir))
+            seg_vol_arr = np.array(Image.open(seg_dir))
+            
+            # Get the amount of segmentation in the image
+            label_amount = np.count_nonzero(seg_vol_arr)
+            if label_amount >= config.get('min_label_amount', 0):
+
+                # Normalize the image to be between 0 and 1
+                img_vol_arr = normalize_image(img_vol_arr)
+
+                # We can visaulize these as an option.
+                if config.get('show_examples', False):
+                    f, axarr = plt.subplots(1, 2, figsize=(10, 5))
+                    im = axarr[0].imshow(img_vol_arr, cmap='gray')
+                    axarr[0].set_title("Image")
+                    f.colorbar(im, ax=axarr[0])
+                    se =axarr[1].imshow(seg_vol_arr, cmap='gray')
+                    axarr[1].set_title("Segmentation")
+                    f.colorbar(se, ax=axarr[1])
+                    plt.show()
+
+                # Get the proportion of the binary mask.
+                gt_prop = np.count_nonzero(seg_vol_arr) / seg_vol_arr.size
+
+                # We actually can have a distinction between samples and subjects!!
+                # Splits are done at the subject level, so we need to keep track of the subjects.
+                db[subj_name] = {
+                    "img": img_vol_arr, 
+                    "seg": seg_vol_arr,
+                    "gt_proportion": gt_prop
+                } 
+                subjects.append(subj_name)
+
+        subjects = sorted(subjects)
+        
+        # Print the number of samples in each split for debugging purposes.
+        for split_key in db_splits:
+            print(f"{split_key}: {len(db_splits[split_key])} samples")
+
+        # Save the metadata
+        attrs = dict(
+            dataset="ISLES",
+            version=version,
+        )
+        db["_subjects"] = subjects
+        db["_samples"] = subjects
+        db["_splits"] = db_splits
+        db["_attrs"] = attrs
+
